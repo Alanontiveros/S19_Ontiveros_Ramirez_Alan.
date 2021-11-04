39f9415665fd0e74ad7513ba8e083adbd59c677d
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-11-04 16:10:07 -0600

    Update T1E3_birthday.py
    Excelente solo el acomodo de los guiones no era el correcto, pero hace lo que se pide

--- Tarea_01/T1E3_birthday.py
+++ Tarea_01/T1E3_birthday.py
@@ -1,11 +1,16 @@
 """Ingresar tu fecha de cumpleaños como enteros en variables por separado
-e imprímela junto al string:
-“Year: xxxx - Month: xx - Day: xx”
-donde las xx son las variables correspondientes."""
-#Dia
-var_año = int(input("Introduce el año en que naciste:"))
-var_mes = int(input("Introduce el mes en que naciste:"))
-var_dia = int(input("Introduce el dia en que naciste:"))
+    e imprímela junto al string:
+    “Year: xxxx - Month: xx - Day: xx”
+    donde las xx son las variables correspondientes."""
 
-print(f" Year: - {var_año} Month: - {var_mes:02d} day: - {var_dia:02d}")
+# En el PDF no les puse este requisito (en el correo si), pero en todas las tarea vamos a incluir esta condición
+if __name__ == '__main__':
+    # Dia
+    var_año = int(input("Introduce el año en que naciste:"))
+    var_mes = int(input("Introduce el mes en que naciste:"))
+    var_dia = int(input("Introduce el dia en que naciste:"))
 
+    # Todo perfecto pero el guion de la impresion va despues del valor
+    # print(f" Year: - {var_año} Month: - {var_mes:02d} day: - {var_dia:02d}")
+    print(f" Year: {var_año} -  Month: {var_mes:02d} -  day: {var_dia:02d}")
+    
