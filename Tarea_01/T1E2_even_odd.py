aa2659c51e1e498c46544e7518115dd4aeba8c7a
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-11-04 16:04:30 -0600

    Update T1E2_even_odd.py
    Perfecto

--- Tarea_01/T1E2_even_odd.py
+++ Tarea_01/T1E2_even_odd.py
@@ -1,11 +1,12 @@
 ##Ingresar un número y validar si es par o impar.
 
-# Excelente, en este caso si tenemos que castear a int, de otra forma no funciona el operador %
-num = input("Introduce un número: ")
-num = int(num)
-if num == 0:
-    print("Este número es nulo.")
-elif num % 2 == 0: # esta linea divide entre dos y retorna el residuo si es igual a 0 es par de lo contrario impar
-    print("Este numero es par")
-else:
-    print("Este numero es impar")
+# En el PDF no les puse este requisito, pero en todas las tarea vamos a incluir esta condición
+if __name__ == '__main__':
+    num = input("Introduce un número: ")
+    num = int(num)  # Excelente, en este caso si tenemos que castear a int, de otra forma no funciona el operador %
+    if num == 0:
+        print("Este número es nulo.")
+    elif num % 2 == 0:  # esta linea divide entre dos y retorna el residuo si es igual a 0 es par de lo contrario impar
+        print("Este numero es par")
+    else:
+        print("Este numero es impar")
