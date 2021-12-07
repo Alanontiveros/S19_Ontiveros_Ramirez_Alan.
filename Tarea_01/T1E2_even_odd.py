d4fe4fe45f5d48a23787be05cc3967411d687cb1
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-12-07 15:46:12 -0600

    Update T1E2_even_odd.py

--- Tarea_01/T1E2_even_odd.py
+++ Tarea_01/T1E2_even_odd.py
@@ -1,19 +1,22 @@
 """
-Date:       2021-11-04
+Date:       2021-11-17
 Author:     Alan Ontiveros Ramirez
 File:       T1E2_even_odd.py
 Brief:      Ingresar un número y validar si es par o impar.
-Score:      100
+Score:      90
 Version:    1.1.1
-Fixes:      -
+Fixes:      - PEP8 recomienda no rebasar los 99 carácteres en una línea 
+                de código, yo establecí en los requerimientos máximo 72
+                carácteres
 """
 
 if __name__ == '__main__':
     num = input("Introduce un número: ")
-    num = int(num)  # Excelente, en este caso si tenemos que castear a int, de otra forma no funciona el operador %
+    num = int(num)  # Excelente, en este caso si tenemos que castear a int, de otra forma no funciona el operador %     # PEP8
     if num == 0:
         print("Este número es nulo.")
-    elif num % 2 == 0:  # esta linea divide entre dos y retorna el residuo si es igual a 0 es par de lo contrario impar
+    elif num % 2 == 0:  # esta linea divide entre dos y retorna el residuo si es igual a 0 es par de lo contrario impar # PEP8
         print("Este numero es par")
     else:
         print("Este numero es impar")
+        
