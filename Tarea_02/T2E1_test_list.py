5aa7c55b5f3979c9ccc89d11110cbbce4faf2f75
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-24 22:16:08 -0600

    Update T2E1_test_list.py

--- Tarea_02/T2E1_test_list.py
+++ Tarea_02/T2E1_test_list.py
@@ -2,12 +2,23 @@
 Date:       2021-11-15
 Authors:    Alan Emmanuel Ontiveros Ramírez
 File:       T2E1_test_list
-Brief:      Crear una lista de n elementos de texto (strings) predefinidos, después realizar los
-            siguientes pasos (comentar el número y nombre del paso) e imprimir la lista en cada paso:
-Score:      -
-Version:    0.0.1
-Fixies:     -
+Brief:      Crear una lista de n elementos de texto (strings)
+            predefinidos, después realizar los siguientes pasos
+            (comentar el número y nombre del paso) e imprimir la lista
+            en cada paso:
+Score:      95
+Version:    1.1.1
+Fixies:     - Le falta la sangria al código a partir de el comentario
+                "# 1 Metodo Append", todo debe estar dentro de la 
+                jerarquia de la condición if __name__ == "__main__":
+
+            - Funcionalmente correcto, pero hubiera sido mejor que 
+            solo definieras la lista my_list una vez y que a partir de
+            ella fueras usando todos sus métodos ya que es lo que se 
+            suele hacer normalmente, para no estar tecleando una nueva
+            definicion cada vez (tecleas más código del necesario) 
 """
+
 if __name__ == '__main__':
 
     my_list = ["Alfa", "Beta", "Gamma", "Delta", "Épsilon"]
